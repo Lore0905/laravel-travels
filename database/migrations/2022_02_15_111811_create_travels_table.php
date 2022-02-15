49f9c99dc966e0d8a0ae06458cf479be69952f51
hexeva <patrick79.g@gmail.com>
2022-02-15 12:25:26 +0100

    created travel migration

---
+++ database/migrations/2022_02_15_111811_create_travels_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTravelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('travels', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',50);
+            $table->string('destination',50);
+            $table->string('vehicle',50);
+            $table->decimal('price',8,2);
+            $table->tinyInteger('number_of_days');
+            $table->timestamps();
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('travels');
+    }
+}
